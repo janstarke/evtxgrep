4cab16f64f9fc9940f0c07c4f67eaa13afff1537
Jan Starke <Jan.Starke@t-systems.com>
2021-04-06 18:31:42 +0200

    better json search performance

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,58 @@
-use argparse::{ArgumentParser, Store};
+use argparse::{ArgumentParser, Store, StoreTrue};
 use simple_logger::SimpleLogger;
 use regex::Regex;
 use anyhow::{Error, Result};
 use evtx::*;
+use evtx::err::EvtxError;
 use std::path::PathBuf;
 use serde_json::{Value};
+use std::string::String;
 
 struct GrepFilters {
     data: Regex,
     id: Regex
 }
 
-impl GrepFilters {
+trait FilterMethod {
+    fn matches(&self, record: &SerializedEvtxRecord<String>) -> bool;
+}
+
+struct XmlFilter {
+    filters: GrepFilters,
+}
+
+struct JsonFilter {
+    filters: GrepFilters,
+}
+
+impl FilterMethod for XmlFilter {
+    fn matches(&self, _record: &SerializedEvtxRecord<String>) -> bool {
+        false
+    }
+}
+
+impl FilterMethod for JsonFilter {
     fn matches(&self, record: &SerializedEvtxRecord<std::string::String>) -> bool {
         let v: Value = serde_json::from_str(&record.data).unwrap();
         let event = &v["Event"];
         let event_id = &event["System"]["EventID"];
-        if ! self.id.is_match(&event_id.to_string()) {
+        if ! self.filters.id.is_match(&event_id.to_string()) {
             return false;
         }
-        //println!("{}", &event["EventData"].to_string());
-        self.data.is_match(&event["EventData"].to_string())
+        Self::matches_value(&self.filters.data, &event["EventData"])
+    }
+}
+
+impl JsonFilter {
+    fn matches_value(regex: &Regex, value: &Value) -> bool {
+        match value {
+            Value::Null         => false,
+            Value::Bool(_)      => false,
+            Value::Number(n)    => regex.is_match(&n.to_string()),
+            Value::String(s)    => regex.is_match(s),
+            Value::Array(a)     => a.iter().fold(false, |m, x| m || Self::matches_value(&regex, x)),
+            Value::Object(o)    => o.values().fold(false, |m, x| m || Self::matches_value(&regex, x))
+        }
     }
 }
 
@@ -30,16 +62,18 @@ fn main() -> Result<()> {
     let mut evtxfile = String::new();
     let mut data = String::new();
     let mut id = String::new();
+    let mut xml_format = false;
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("regular expression based search in Windows Event Log files");
         ap.refer(&mut evtxfile).add_argument("evtxfile", Store, "name of the evtx file").required();
-        ap.refer(&mut id).add_option(&["-I", "--id"], Store, "pattern used to filter event ids");
+        ap.refer(&mut xml_format).add_option(&["-X", "--xml"], StoreTrue, "use XML format instead of JSON");
         ap.refer(&mut data).add_option(&["-D", "--data"], Store, "pattern to search for in the data section");
+        ap.refer(&mut id).add_option(&["-I", "--id"], Store, "pattern used to filter event ids");
         ap.parse_args_or_exit();
     }
 
-    let filter = GrepFilters {
+    let filters = GrepFilters {
         data: Regex::new(&data)?,
         id: Regex::new(&id)?
     };
@@ -49,13 +83,29 @@ fn main() -> Result<()> {
         return Err(Error::msg(format!("File {} does not exist", &evtxfile)));
     }
     
-    let mut parser = EvtxParser::from_path(fp)?;
-    for record in parser.records_json()
-            .filter(|r| match r {
-                Ok(_) => filter.matches(r.as_ref().unwrap()),
-                Err(e) => {log::warn!("parser error: {}", e); false} }) {
-                print_record(&record.unwrap());
-            }
+    let settings = ParserSettings::default().num_threads(0);
+    let parser = EvtxParser::from_path(fp)?;
+    let mut parser = parser.with_configuration(settings);
+
+    let records: Box<dyn Iterator<Item = Result<SerializedEvtxRecord<String>, EvtxError>>> = if xml_format {
+        Box::new(parser.records())
+    } else {
+        Box::new(parser.records_json())
+    };
+
+    let filter_method: Box<dyn FilterMethod> = if xml_format {
+        Box::new(XmlFilter {filters})
+    } else {
+        Box::new(JsonFilter {filters})
+    };
+
+    let records = records.filter(|r| match r {
+        Ok(_) => filter_method.matches(r.as_ref().unwrap()),
+        Err(e) => {log::warn!("parser error: {}", e); false}
+    });
+    for record in records {
+        print_record(&record.unwrap());
+    }
     Ok(())
 }
 
