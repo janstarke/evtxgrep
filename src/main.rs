960966927dafba9f315574a55894c39d0cd5bfd1
Jan Starke <jan.starke@outofbed.org>
2021-06-13 17:31:40 +0200

    minimal working example

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,12 @@
 use argparse::{ArgumentParser, Store};
 use simple_logger::SimpleLogger;
-use regex::Regex;
 use anyhow::{Error, Result};
 use evtx::*;
 use std::path::PathBuf;
 use std::string::String;
-use crate::visitor::*;
 use crate::xml_visitor::*;
-use crate::xml_output_visitor::XmlOutputVisitor;
 
-mod visitor;
 mod xml_visitor;
-mod xml_output_visitor;
-
-#[allow(unused)]
-struct GrepFilters {
-    data: Regex,
-    id: Regex
-}
-
-trait FilterMethod {
-    fn matches(&self, record: &EvtxStructure) -> bool;
-}
-
-#[allow(unused)]
-struct XmlFilter {
-    filters: GrepFilters,
-}
-
-#[allow(unused)]
-struct JsonFilter {
-    filters: GrepFilters,
-}
-
-#[allow(unused)]
-impl FilterMethod for XmlFilter {
-
-    fn matches(&self, _record: &EvtxStructure) -> bool {
-        true
-    }
-}
 
 fn main() -> Result<()> {
     SimpleLogger::new().with_level(log::LevelFilter::Warn).init().unwrap();
@@ -56,11 +23,6 @@ fn main() -> Result<()> {
         ap.parse_args_or_exit();
     }
 
-    let filters = GrepFilters {
-        data: Regex::new(&data)?,
-        id: Regex::new(&id)?
-    };
-
     let fp = PathBuf::from(&evtxfile);
     if ! (fp.exists() && fp.is_file()) {
         return Err(Error::msg(format!("File {} does not exist", &evtxfile)));
@@ -70,35 +32,17 @@ fn main() -> Result<()> {
     let parser = EvtxParser::from_path(fp)?;
     let mut parser = parser.with_configuration(settings);
 
-    let records = parser.records_struct();
-    let filter_method = XmlFilter {filters};
-
-    let mut records: Vec<EvtxStructure> = records.filter_map(|r| match r {
-        Ok(s) => if filter_method.matches(&s) {
-            Some (s)
-        } else {
-            None
-        },
-        Err(e) => {log::warn!("parser error: {}", e); None}
-    }).collect();
-    records.sort_unstable();
-    let line_printer = LinePrinter{};
-    let mut visitor = XmlOutputVisitor::new(&line_printer);
+    let records = parser.records_to_visitor(XmlVisitorBuilder::new()).filter_map(|r|
+        match r {
+            Ok(x) => Some(x),
+            Err(e) => {log::warn!("parser error: {}", e); None}
+        }
+    );
 
     for record in records {
-        let mut xml_visitor = XmlVisitor::new();
-        record.visit_structure(&mut xml_visitor);
-        println!("{}", xml_visitor.to_string());
-
-        //record.visit_structure(&mut visitor);
+        if let Some(s) = record {
+            println!("{}", s);
+        }
     }
     Ok(())
-}
-
-struct LinePrinter {}
-
-impl LineOutput for LinePrinter {
-    fn println(&self, line: &str) {
-        println!("{}", line);
-    }
 }
--- src/xml_visitor.rs
+++ src/xml_visitor.rs
@@ -1,75 +1,124 @@
-use evtx::{EvtxStructureVisitor};
-use std::collections::hash_map::Iter;
-use std::cell::{Ref, RefCell};
-use crate::visitor::LineOutput;
+use evtx::{EvtxStructureVisitor, VisitorBuilder};
 use libxml::tree::document::{Document, SaveOptions};
 use libxml::tree::node::Node;
-use libxml::tree::namespace::Namespace;
+
+#[derive(Clone)]
+pub struct XmlVisitorBuilder {
+}
+
+impl XmlVisitorBuilder {
+  pub fn new() -> Self {
+    Self {
+    }
+  }
+}
+
+impl VisitorBuilder<Option<String>> for XmlVisitorBuilder {
+  fn build(&self) -> Box<dyn EvtxStructureVisitor<VisitorResult=Option<String>>> {
+    Box::new(XmlVisitor::new())
+  }
+}
 
 pub struct XmlVisitor {
-    doc: Document,
-    stack: Vec<Node>,
+  doc: Document,
+  stack: Vec<Node>,
 }
 
 impl XmlVisitor {
-    pub fn new() -> Self {
-        let mut doc = Document::new().unwrap();
-        let mut stack = Vec::new();
+  pub fn new() -> Self {
+    let mut doc = Document::new().unwrap();
+    let mut stack = Vec::new();
 
-        let root = Node::new("Event", None, &doc).unwrap();
-        doc.set_root_element(&root);
-        stack.push(root);
+    let root = Node::new("Event", None, &doc).unwrap();
+    doc.set_root_element(&root);
+    stack.push(root);
 
-        Self {
-            doc,
-            stack
-        }
-    }
-
-    fn set_attributes(node: &mut Node, attributes: Iter<String, String>) {
-        for (key, value) in attributes {
-            node.set_attribute(key, value).unwrap();
-        }
-    }
+    Self { doc, stack }
+  }
 }
 
 impl ToString for XmlVisitor {
-    fn to_string(&self) -> String {
-        let mut options = SaveOptions::default();
-        options.as_xml = true;
-        options.no_declaration = true;
-        options.format = true;
-        self.doc.to_string_with_options(options)
-    }
+  fn to_string(&self) -> String {
+    let mut options = SaveOptions::default();
+    options.as_xml = true;
+    options.no_declaration = true;
+    options.format = true;
+    self.doc.to_string_with_options(options)
+  }
 }
 
 impl EvtxStructureVisitor for XmlVisitor {
-    fn visit_empty_element(&mut self, name: &str, attributes: Iter<String, String>) {
-        let mut node = self.stack.last_mut().unwrap().add_text_child(
-            None,
-            name,
-            ""
-        ).unwrap();
-        XmlVisitor::set_attributes(&mut node, attributes);
-    }
+  type VisitorResult = Option<String>;
 
-    fn visit_simple_element(&mut self, name: &str, attributes: Iter<String, String>, content: &str) {
-        let mut node = self.stack.last_mut().unwrap().add_text_child(
-            None,
-            name,
-            content
-        ).unwrap();
-        XmlVisitor::set_attributes(&mut node, attributes);
-    }
+  fn get_result(&self) -> Self::VisitorResult {
+    Some(self.to_string())
+  }
 
-    fn visit_start_element(&mut self, name: &str, attributes: Iter<String, String>) {
-        let mut node = Node::new(name, None, &self.doc).unwrap();
-        XmlVisitor::set_attributes(&mut node, attributes);
-        self.stack.last_mut().unwrap().add_child(&mut node);
-        self.stack.push(node);
-    }
+  /// called when a new record starts
+  fn start_record(&mut self) {}
 
-    fn visit_end_element(&mut self, name: &str) {
-        self.stack.pop();
+  /// called when the current records is finished
+  fn finalize_record(&mut self) { }
+
+  // called upon element content
+  fn visit_characters(&mut self, _value: &str) {}
+
+  fn visit_empty_element<'a, 'b>(
+    &'a mut self,
+    name: &'b str,
+    attributes: Box<dyn Iterator<Item = (&'b str, &'b str)> + 'b>,
+  ) where
+    'a: 'b,
+  {
+    let mut node = self
+      .stack
+      .last_mut()
+      .unwrap()
+      .add_text_child(None, name, "")
+      .unwrap();
+
+    for (key, value) in attributes {
+      node.set_attribute(key, value).unwrap();
     }
+  }
+
+  fn visit_simple_element<'a, 'b>(
+    &'a mut self,
+    name: &'b str,
+    attributes: Box<dyn Iterator<Item = (&'b str, &'b str)> + 'b>,
+    content: &'b str,
+  ) where
+    'a: 'b,
+  {
+    let mut node = self
+      .stack
+      .last_mut()
+      .unwrap()
+      .add_text_child(None, name, content)
+      .unwrap();
+    for (key, value) in attributes {
+      node.set_attribute(key, value).unwrap();
+    }
+  }
+
+  fn visit_start_element<'a, 'b>(
+    &'a mut self,
+    name: &'b str,
+    attributes: Box<dyn Iterator<Item = (&'b str, &'b str)> + 'b>,
+  ) where
+    'a: 'b,
+  {
+    let mut node = Node::new(name, None, &self.doc).unwrap();
+
+    for (key, value) in attributes {
+      node.set_attribute(key, value).unwrap();
+    }
+    
+    let _ = self.stack.last_mut().unwrap().add_child(&mut node);
+    self.stack.push(node);
+  }
+
+  fn visit_end_element(&mut self, _name: &str) {
+    self.stack.pop();
+  }
 }
